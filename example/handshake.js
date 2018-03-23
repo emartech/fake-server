244b11e08b387e16118e157ee76266b61e754977
gergaczd <gergaczdaniel@gmail.com>
2018-03-23 13:30:01 +0100

    feat(authentication): add support for escher authentication

--- example/handshake.js
+++ example/handshake.js
@@ -8,7 +8,13 @@ const jwtToken = jwt.sign({ user: 'test user' }, jwtSecret);
 const app = FakeServer.createApp([
   {
     url: '/handshake',
-    response: { payload: jwtToken }
+    response: { payload: jwtToken },
+    authentication: {
+      escher: {
+        credentialScope: 'eu/my-service/test_request',
+        keyPool: JSON.stringify([{ keyId: 'my-key-id_v1', secret: 'my-secret', acceptOnly: 0 }])
+      }
+    }
   },
   {
     url: '/get-list',
