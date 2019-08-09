b85def2dcfdd54f315cb3f36ccd0cb2888df2376
Norbert Varga <draconteus@gmail.com>
2019-08-09 14:16:53 +0200

    feat(sinon): update to latest version with chai

--- test-setup.spec.js
+++ test-setup.spec.js
@@ -1,18 +1,13 @@
 'use strict';
 
-let sinon = require('sinon');
-
-let chai = require('chai');
-let sinonChai = require('sinon-chai');
+const sinon = require('sinon');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
 
 chai.use(sinonChai);
 global.expect = chai.expect;
 global.sinon = sinon;
 
-beforeEach(function() {
-  this.sandbox = sinon.sandbox.create();
-});
-
 afterEach(function() {
-  this.sandbox.restore();
+  sinon.restore();
 });
